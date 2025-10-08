6a4961cf7259aa9e4081c06e9ba6e6488b031ea2
ValyaSorokivska <valya.sorokivska@gmail.com>
2025-10-08 23:14:33 +0200

    Exercise 1 finall commit

--- exercise_session_03/cpi_mpi.c
+++ exercise_session_03/cpi_mpi.c
@@ -1,41 +1,43 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
-#include "mpi.h"
+#include <mpi.h>
+#include "gettime.h"
 
-static long nSteps = 1000000000;
+static long   nSteps = 100000000;                // 1e8 steps; adjust if needed
 static double PI25DT = 3.141592653589793238462643;
 
-int main(int argc, char** argv){
-    int n, myRank, nProcs;
-    long i;
-    double mypi, pi, h, sum, x;
-    double tWStart, tWElapsed;
+int main(int argc, char **argv)
+{
+    double prog0 = getTime();                    // total program start
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD,&nProcs);
-    MPI_Comm_rank(MPI_COMM_WORLD,&myRank);
+    double mpi0  = getTime();                    // MPI-only start
 
-    printf("Process rank: %d\n", myRank);
-  
-	h   = 1.0 / (double) nSteps;
-	sum = 0.0;
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    tWStart = MPI_Wtime();
+    double h   = 1.0 / (double)nSteps;
+    double sum = 0.0, x;
 
-	for (i = myRank ; i < nSteps; i += nProcs) {
-	    x = h * ((double)i - 0.5);
-	    sum += 4.0 / (1.0 + x*x);
-	}
-	mypi = h * sum;
-    
-	MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0,
-		   MPI_COMM_WORLD);
-    
-    tWElapsed = MPI_Wtime() - tWStart;
-	if (myRank == 0){
-	    printf("pi is approximately %.16f, Error is %.16f\n",
-		        pi, pi - PI25DT);
-        printf("Computed in %.4g seconds\n", tWElapsed);
+    // block-cyclic over i
+    for (long i = rank; i < nSteps; i += size) {
+        x = h * ((double)i - 0.5);
+        sum += 4.0 / (1.0 + x * x);
     }
+
+    double global_sum = 0.0;
+    MPI_Reduce(&sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    double mpi1  = getTime();                    // MPI-only end
     MPI_Finalize();
+    double prog1 = getTime();                    // total program end
+
+    if (rank == 0) {
+        double pi = h * global_sum;
+        printf("pi is approximately %.16f, Error is %.16f\n", pi, pi - PI25DT);
+        printf("Program time (incl. MPI_Init/Finalize): %.6f s\n", prog1 - prog0);
+        printf("MPI-only time (excl. Init/Finalize):    %.6f s\n",  mpi1 - mpi0);
+    }
     return 0;
 }

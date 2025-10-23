aa9acdf23c670b9e51e5b5c57fa070fd345f4832
ValyaSorokivska <131530931+ValyaSorokivska@users.noreply.github.com>
2025-10-23 20:10:46 +0200

    Update io.h

--- exercise_session_05/poisson_solver/io.h
+++ exercise_session_05/poisson_solver/io.h
@@ -4,6 +4,8 @@
 #include "init.h"
 #include <fstream>
 
+void write_csv(const char* path, double** A, int nx, int ny);
+
 void output_source(params p, double** f);
 void output(params p, int step, double** u_new);
 

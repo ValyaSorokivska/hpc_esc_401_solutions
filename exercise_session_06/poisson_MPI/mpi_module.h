94ee82efedaff74b027134788ee479e73d534756
ValyaSorokivska <131530931+ValyaSorokivska@users.noreply.github.com>
2025-11-07 12:30:55 +0100

    fixed small error

--- exercise_session_06/poisson_MPI/mpi_module.h
+++ exercise_session_06/poisson_MPI/mpi_module.h
@@ -11,4 +11,3 @@ int mpi_get_domain(int nx, int ny, int my_rank, int size, int* min_x, int* max_x
 int halo_comm(params p, int my_rank, int size, double** u, double* fromLeft, double* fromRight);
 int ALLREDUCE(double* loc_diff, double* loc_sumdiff);
 
-#endif

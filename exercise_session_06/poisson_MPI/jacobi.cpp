94d132c0b0368d9baf77c3b0fbf16480431ab7b9
ValyaSorokivska <131530931+ValyaSorokivska@users.noreply.github.com>
2025-11-07 10:44:54 +0100

    halo exchange on u_old

--- exercise_session_06/poisson_MPI/jacobi.cpp
+++ exercise_session_06/poisson_MPI/jacobi.cpp
@@ -13,16 +13,22 @@
  */
 double norm_diff(params p, double** mat1, double** mat2){
 
-    printf("Here, in norm_diff() function, change the serial implementation to MPI setup\n");
-    double ret=0., diff=0.;
-    for (int i=0; i<p.nx; i++){
-        for (int j=0; j<p.ny; j++){
-            diff = mat1[i][j] - mat2[i][j];
-            ret += diff*diff;
+    double loc = 0.0;
+    for (int i = p.xmin; i < p.xmax; ++i) {
+        for (int j = p.ymin; j < p.ymax; ++j) {
+            const int ix = i - p.xmin;
+            const int iy = j - p.ymin;
+            const double d = mat1[ix][iy] - mat2[ix][iy];
+            loc += d * d;
         }
     }
-    ret = sqrt(ret/(p.nx*p.ny));
-    return ret;
+
+    // global reduction
+    double glob = 0.0;
+    ALLREDUCE(&loc, &glob);   
+
+    // over entire grid
+    return sqrt( glob / ( (double)p.nx * (double)p.ny ) );
 }
 
 /**
@@ -41,22 +47,36 @@ void jacobi_step(params p, double** u_new, double** u_old, double** f, int my_ra
     double* fromLeft = new double[p.ymax - p.ymin]; 
     double* fromRight = new double[p.ymax - p.ymin];
 
-    for (int i=p.xmin; i<p.xmax; i++){
-        for (int j=p.ymin; j<p.ymax; j++){
+    for (int i = p.xmin; i < p.xmax; ++i)
+        for (int j = p.ymin; j < p.ymax; ++j)
             u_old[i - p.xmin][j - p.ymin] = u_new[i - p.xmin][j - p.ymin];
-        }
-    }
 
-    halo_comm(p, my_rank, size, u_new, fromLeft, fromRight); 
+    
+    halo_comm(p, my_rank, size, u_old, fromLeft, fromRight);
 
-    printf("Function jacobi_step in jacobi.cpp : adapt the update of u_new.\n");
-    for (int i=p.xmin; i<p.xmax; i++){
-        if (i==0 || i==p.nx-1) continue;
-        for (int j=p.ymin; j<p.ymax; j++){
-            if (j==0 || j==p.ny-1) continue;
-            int idx = i-p.xmin;
-            int idy = j-p.ymin;
-            u_new[idx][idy] = 0.25*(u_old[idx-1][idy] + u_old[idx+1][idy] + u_old[idx][idy-1] + u_old[idx][idy+1] - dx*dy*f[idx][idy]);
+    for (int i = p.xmin; i < p.xmax; ++i) {
+        if (i == 0 || i == p.nx - 1) continue;
+        for (int j = p.ymin; j < p.ymax; ++j) {
+            if (j == 0 || j == p.ny - 1) continue;
+
+            const int ix = i - p.xmin;
+            const int iy = j - p.ymin;
+
+            //handle slab edges 
+            double leftN, rightN;
+            if (ix == 0)            leftN  = fromLeft[iy];
+            else                    leftN  = u_old[ix - 1][iy];
+
+            if (ix == nx_loc - 1)   rightN = fromRight[iy];
+            else                    rightN = u_old[ix + 1][iy];
+
+            // neighbors in y are inside the same slab
+            double downN = u_old[ix][iy - 1];
+            double upN   = u_old[ix][iy + 1];
+
+            //new formula
+            u_new[ix][iy] = 0.25 * ( leftN + rightN + downN + upN
+                                     - dx * dy * f[ix][iy] );
         }
     }
     if (p.nx!=p.ny) printf("In function jacobi_step (jacobi.cpp l.26): nx != ny, check jacobi updates\n");

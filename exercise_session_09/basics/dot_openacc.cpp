4a4b9a42b07a9a8e3920a6c6f9eb7b5b0738276f
ValyaSorokivska <131530931+ValyaSorokivska@users.noreply.github.com>
2025-11-21 12:35:29 +0100

    parallelized with OpenACC

--- exercise_session_09/basics/dot_openacc.cpp
+++ exercise_session_09/basics/dot_openacc.cpp
@@ -7,7 +7,7 @@
 double dot_host(const double *x, const double *y, int n) {
     double sum = 0;
 
-    #pragma omp parallel for reduction(+:sum)
+    #pragma omp parallel for reduction(+:sum) 
     for (int i = 0; i < n; ++i) {
         sum += x[i]*y[i];
     }
@@ -19,7 +19,7 @@ double dot_gpu(const double *x, const double *y, int n) {
     double sum = 0;
     int i;
 
-    // TODO: Offload this loop to the GPU
+    #pragma acc parallel loop reduction(+:sum) copyin(x[0:n], y[0:n])
     for (i = 0; i < n; ++i) {
         sum += x[i]*y[i];
     }
